d2795b64a1673e7cc3ae2d3086c3431287691470
unknown <mjogaret@yahoo.fr>
2018-12-07 19:19:43 +0100

    Envoi code de appli qui ne fonctionne pas pour le moment dans la recherche

---
+++ Navigation/Navigation.js
@@ -0,0 +1,97 @@
+// Navigation/Navigation.js
+
+import React from 'react' // N'oubliez pas l'import de React ici. On en a besoin pour rendre nos components React Native Image !
+import { StyleSheet, Image } from 'react-native';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation'
+import Search from '../Components/Search'
+import FilmDetails from '../Components/FilmDetails'
+import Favorites from '../Components/Favorites'
+import News from '../Components/News'
+
+const SearchStackNavigator = createStackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: 'Rechercher'
+    }
+  },
+  FilmDetails: {
+    screen: FilmDetails
+  }
+})
+
+const FavoritesStackNavigator = createStackNavigator({
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      title: 'Favoris'
+    }
+  },
+  FilmDetails: {
+    screen: FilmDetails
+  }
+})
+
+const NewsStackNavigator = createStackNavigator({
+  News: {
+    screen: News,
+    navigationOptions: {
+      title: 'Les Derniers Films',
+    },
+  },
+  FilmDetails: {
+    screen: FilmDetails,
+  }
+})
+
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+          return <Image
+            source={require('../Images/ic_search.png')}
+            style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+        }
+      }
+    },
+    Favorites: {
+      screen: FavoritesStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_favorite.png')}
+            style={styles.icon}/>
+        }
+      }
+    },
+    News: {
+      screen: NewsStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return <Image
+            source={require('../Images/ic_fiber_new.png')}
+            style={styles.icon}/>
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+      inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+      showLabel: false, // On masque les titres
+      showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default createAppContainer(MoviesTabNavigator)
